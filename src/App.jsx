97e33efc3610648f723473b31df515acff2aad48
Henrique Douglas <henriquedouglas2803@gmail.com>
2025-07-21 19:48:19 -0300

    adiciona componente ItemFeedback

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import "./App.css";
 import ItemReceita from "./components/ItemCardapio/ItemCardapio";
 import Header from "./components/Header/Header";
 import ContactUs from "./components/Contact/ContactUs";
+import ItemFeedback from "./components/ItemFeedback/ItemFeedback";
 
 function App() {
   return (
     <div>
-      
       <Header />
 
       <section className="container-cardapio" id="cardapio">
@@ -40,11 +40,46 @@ function App() {
           />
         </div>
       </section>
-      
+
       <ContactUs />
 
-
-     
+      <section className="container-feedback">
+        <h2>Feedback dos clientes</h2>
+        <ul>
+          <ItemFeedback
+            author="Douglas"
+            comment="Lorem ipsum lore impsum Lorem ipsum lore impsum Lorem ipsum lore impsum "
+          />
+          <ItemFeedback
+            author="Maria"
+            comment="Adorei as empadas, muito saborosas!"
+          />
+          <ItemFeedback
+            author="João"
+            comment="Atendimento excelente e entrega rápida."
+          />
+          <ItemFeedback
+            author="Ana"
+            comment="Empada de queijo é minha favorita!"
+          />
+          <ItemFeedback
+            author="Carlos"
+            comment="Recomendo para todos os meus amigos."
+          />
+          <ItemFeedback
+            author="Fernanda"
+            comment="Variedade incrível no cardápio."
+          />
+          <ItemFeedback
+            author="Paulo"
+            comment="Muito bom, voltarei a comprar."
+          />
+          <ItemFeedback
+            author="Juliana"
+            comment="Ótima experiência, parabéns!"
+          />
+        </ul>
+      </section>
     </div>
   );
 }
---
+++ src/components/ItemFeedback/ItemFeedback.jsx
@@ -0,0 +1,16 @@
+import './ItemFeedback.css'
+
+function ItemFeedback({author, comment}) {
+  return (
+    <li className='item-feedback'>
+      <article>
+        <header>{author}</header>
+        <p>
+          {comment}
+        </p>
+      </article>
+    </li>
+  );
+}
+
+export default ItemFeedback;
